ce0b8b5529d5b1cfc1b4abc5825fbcc4e619d4d0
AUSMcGill <communications.aus@mail.mcgill.ca>
2026-03-17 23:54:22 -0400

    Remove AboutPage, AcademicsPage, and ContactPage components from the build assets.

---
+++ app/page.tsx
@@ -0,0 +1,6 @@
+import { HomePage } from '../src/components/pages/HomePage';
+
+export default function Page() {
+  return <HomePage />;
+}
+
--- build/assets/icons-DtJAa8oY.js
+++ build/assets/icons-DtJAa8oY.js
@@ -1,4 +1,4 @@
-import{r as s}from"./react-DZA_LsP3.js";/**
+import{r as s}from"./react-DQm02LSL.js";/**
  * @license lucide-react v0.487.0 - ISC
  *
  * This source code is licensed under the ISC license.
